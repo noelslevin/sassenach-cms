2dd767fc7d45afb285936a8aa7baca2f336d4bd5
Noel Slevin <noelslevin@gmail.com>
2015-11-07 14:36:23 +0000

    Fixed typo
    Fixed a small typo on the page.

--- backend/user/profile.php
+++ backend/user/profile.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "<p>Please use this page to edit your profile. Some of the information in this profile will bwe displayed to other members on the site.</p>";
+echo "<p>Please use this page to edit your profile. Some of the information in this profile will be displayed to other members on the site.</p>";
 
 ?>
