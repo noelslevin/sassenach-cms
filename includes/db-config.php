5facc5af08e487b2c23cf6b926f84fbce4b25cfc
Noel Slevin <noelslevin@gmail.com>
2020-08-27 11:58:25 +0100

    Replaced database connection file
    Removed the old MySQL connection file using the mysql driver and replaced with a configuration file using PHP constants.
    
    Database connection is handled by the database class.

---
+++ includes/db-config.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+
+Database configuration
+
+*/
+
+// Hostname (almost always localhost)
+define( 'DB_HOST', 'localhost' );
+
+// Username for database
+define( 'DB_USER', '' );
+
+// Password for username accessing database
+define( 'DB_PASS', '' );
+
+// Database name
+define( 'DB_NAME', '' );
+
+?>
